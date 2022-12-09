87cd6765689643a4238a3324ebb551d9ecdbd623
Joel Rainear-Wills <joel.rainear-wills@mastery.net>
2022-12-09 17:11:22 -0600

    day 9

---
+++ 2022/1209.js
@@ -0,0 +1,70 @@
+const { writeFileSync } = require('fs');
+const { parseArray } = require('../helpers');
+
+const moveUp = (H) => H[1]++;
+const moveDown = (H) => H[1]--;
+const moveLeft = (H) => H[0]--;
+const moveRight = (H) => H[0]++;
+
+const adjustT = (H, T) => {
+    const isAbove = H[1] > T[1];
+    const isRight = H[0] > T[0];
+    const changeX = H[0] - T[0];
+    const changeY = H[1] - T[1];
+    if ((Math.abs(changeY) > 1 && changeX !== 0) || Math.abs(changeX) > 1) {
+        T[0] += (isRight ? 1 : -1);
+    }
+
+    if ((Math.abs(changeX) > 1 && changeY !== 0) || Math.abs(changeY) > 1) {
+        T[1] += (isAbove ? 1 : -1);
+    }
+};
+
+const move = (dir, H) => {
+    switch (dir) {
+        case 'U':
+            return moveUp(H);
+        case 'D':
+            return moveDown(H);
+        case 'L':
+            return moveLeft(H);
+        case 'R':
+            return moveRight(H);
+    }
+};
+
+const simulate = async () => {
+    const instructions = await parseArray('./1209_data.txt', (line) => line.split(' ').map(
+        (value, index) => index === 1 ? parseInt(value) : value)
+    );
+
+    let knots = [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+    ];
+
+    let n = knots.length;
+    let tMoves = new Set();
+    instructions.forEach(([dir, steps]) => {
+        while (steps) {
+            move(dir, knots[0]);
+            for (let i = 0; i < n - 1; ++i) {
+                adjustT(knots[i], knots[i + 1]);
+            }
+            tMoves.add(`${knots[knots.length - 1]}`);
+            steps--;
+        }
+    });
+
+    console.log(tMoves.size);
+};
+
+simulate().catch(err => console.log(err));
